095dd9962fc9e1d401608b6d94d58e1517a29e9e
shireen <syed.shireen@everest.engineering>
2022-05-09 18:27:56 +0530

    Backend code added to application.

---
+++ src/main/java/com/everestengineering/employeeportalapplication/frontend/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import EmployeeComponent from './components/EmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import AddEmployeeComponent from './components/AddEmployeeComponent';
+import UpdateEmployeeComponent from './components/UpdateEmployeeComponent';
+import Employee from './components/Employee'
+
+function App() {
+
+  return (
+    <div >
+        <Router>
+              <HeaderComponent />
+                <div className="container">
+                    <Routes> 
+                          <Route path = "/" exact element= {<EmployeeComponent/>}></Route>
+                          <Route path = "/add" element = {<AddEmployeeComponent/>}></Route>
+                          <Route path="/edit/:id" element={<UpdateEmployeeComponent/>}></Route>
+                          <Route exact path="/:id" element={<Employee/>} ></Route>
+                        
+                    </Routes>
+                </div>
+              <FooterComponent />
+        </Router>
+    </div>
+    
+  );
+}
+export default App;
